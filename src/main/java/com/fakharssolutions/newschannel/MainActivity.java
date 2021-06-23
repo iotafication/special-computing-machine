ade4080d3b1ae8c02f739c2a76082f0eadc6d606
iotafication <51205609+iotafication@users.noreply.github.com>
2021-06-23 13:21:24 +0500

    Add files via upload
    Can't redirect  to whatsapp android application, Error is  ERR_UNKNOWN_URL_SCHEME

---
+++ src/main/java/com/fakharssolutions/newschannel/MainActivity.java
@@ -0,0 +1,51 @@
+package com.fakharssolutions.newschannel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mywebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("https://www.annewsindia.com/");
+        WebSettings webSettings=mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+    public class mywebClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon){
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view,String url){
+            if(url.startsWith("whatsapp:")){
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed(){
+        if(mywebView.canGoBack()) {
+            mywebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
